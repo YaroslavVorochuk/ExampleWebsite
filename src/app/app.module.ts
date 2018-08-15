a038543debe102fc52ad33bf2f913d4020e1143c
YaroslavVorochuk <38429499+YaroslavVorochuk@users.noreply.github.com>
2018-08-15 23:32:15 +0300

    Some commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ChartsComponent } from './charts/charts.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from '../../node_modules/@angular/forms';
+
 
 
 @NgModule({
@@ -18,6 +21,8 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
